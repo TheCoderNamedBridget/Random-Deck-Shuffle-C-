6200477bc6ec99055cebcb20ef463a8bff8796ba
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2020-09-14 10:14:42 -0700

    Create Card.cpp

---
+++ Card.cpp
@@ -0,0 +1,16 @@
+
+
+#include "Card.h"
+#include <iostream>
+using namespace std;
+
+Card::Card()
+{
+    
+}
+
+Card::Card ( string rank, string suit )
+{
+    theRank = rank;
+    theSuit = suit;
+}
